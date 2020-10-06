de189ffb03d111ef6062096ac2910f2dc6cf6b19
Erick Castille <ecastille924@gmail.com>
2020-10-06 01:30:40 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -8,9 +8,8 @@ attr_accessor :backed_projects
     @backed_projects = []
   end
   
-  def back_project(project, backer)
+  def back_project(project)
     @backed_projects << project
-    @backed_projects << backer
   end
 
 end
